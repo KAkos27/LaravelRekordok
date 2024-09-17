040ca4e50a9b0d6d5b80678b6772dec87222eefe
KAkos27 <akoskosztolanyi@gmail.com>
2024-09-17 13:43:58 +0200

    rekordok felv

--- education/database/migrations/0001_01_01_000000_create_users_table.php
+++ education/database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -35,6 +36,24 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+
+        User::create([
+            "name" => "Teszt Elek",
+            "email" => "teszt1@gmail.com",
+            "password" => "aaa",
+        ]);
+
+        User::create([
+            "name" => "Teszt Hanna",
+            "email" => "teszt2@gmail.com",
+            "password" => "aaa1",
+        ]);
+
+        User::create([
+            "name" => "Teszt Peti",
+            "email" => "teszt3@gmail.com",
+            "password" => "aaa213",
+        ]);
     }
 
     /**
